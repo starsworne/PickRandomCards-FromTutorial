e3fc57cf55d81d3bd9189a4b2a06e25676383ffc
♯ Windblade <cambria.shepard@gmail.com>
2021-10-17 18:59:35 -0700

    Add project files.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
